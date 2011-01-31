2062fd0f87457b03668645b095fdbc2a1099bd05
Mik_Die <MikDiet@gmail.com>
2011-01-31 17:08:02 +0300

    Let's rock!! \m/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rock::Application.config.secret_token = '452f61d91a6ca6424d7f23eaf2c0160721896d9dab56a48f88007419f2541fba3c5612cccb0e0e86c544a4ed91ce42e73a724da7392ffa76d3650e3e25c3272d'
