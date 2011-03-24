ab5f7590c34da82977a7838c8f5dc29aad085734
Mik-Die <MikDiet@gmail.com>
2011-03-24 21:49:51 +0700

    all admin controllers must be inherited from Admin::IndexController

--- app/controllers/admin/index_controller.rb
+++ app/controllers/admin/index_controller.rb
@@ -1,7 +1,12 @@
 class Admin::IndexController < ApplicationController
   layout 'admin'
+  before_filter :check_admin
   
   def index
-    render :sign_in unless current_user and current_user.access_level>=50
+  end
+  
+  private
+  def check_admin
+    render 'admin/index/sign_in' unless current_user and current_user.access_level>=50
   end
 end
