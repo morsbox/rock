3bbdd211e20cd179356242b96864f0aec567ee5a
Mik-Die <MikDiet@gmail.com>
2011-03-31 00:44:47 +0800

    added seed with superadmin user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-StaticPage.create :title => "Welcome!", :content => "<p>Welcome to the
-    <strong>Rock'n'RoR!</strong></p>", :order_on_main=>1, :url_match => "home"
+if StaticPage.where(:url_match => "home").empty?
+  StaticPage.create :title => "Welcome!",
+                    :content => "<p>Welcome to the <strong>Rock'n'RoR!</strong></p>",
+                    :order_on_main=>1, :url_match => "home"
+end
+
+if User.where(:username => "admin").empty?
+  user = User.new  :email => "admin@domain.com", :username => "admin",
+                   :password => "123456"
+  user.access_level = 100
+  user.save
+end
