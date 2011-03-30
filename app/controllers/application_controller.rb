a52f96a36a40e4927a4e9cc3f6ac53be325e7427
Mik-Die <MikDiet@gmail.com>
2011-03-31 00:20:53 +0800

    only redirect on admin zone when key :admin_zone assigned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def after_sign_in_path_for(resource_or_scope)
-    admin_root_url
+    if params[:admin_zone]
+      admin_root_url
+    else
+      super
+    end
   end
   
   def after_sign_out_path_for(resource_or_scope)
-    admin_root_url
+    if params[:admin_zone]
+      admin_root_url
+    else
+      super
+    end
   end
 end
