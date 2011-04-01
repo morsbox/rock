f3ed26b5a906e59df28a8c4c57f49509a1c64364
Mik_Die <MikDiet@gmail.com>
2011-04-02 06:09:00 +0800

    added some comments

--- app/models/sider_type.rb
+++ app/models/sider_type.rb
@@ -4,6 +4,8 @@ class SiderType < ActiveRecord::Base
   attr_accessor :default_params
   after_find :load_params
   
+  # this class method returns hash wIA like
+  # :name => {:module => <SiderHelpers::SiderHelper>, :model => <InstanceOfSiderType>}
   def self.list_of_all_helpers
     list = HashWithIndifferentAccess.new{{}}
     SiderHelpers.constants.each do |sh|
@@ -11,11 +13,13 @@ class SiderType < ActiveRecord::Base
       list[sh] = {:module => sh_module} if sh_module.class==Module
     end
     all.each do |st|
+      # Hash#merge! not works in this case properly..
       list[st.sider_helper_module] = list[st.sider_helper_module].merge :model => st
     end
     list
   end
   
+  # returns sider helper module by simple name or nil if it's not present
   def self.to_helper_module(name)
     eval "SiderHelpers::"+name.to_s
   rescue
