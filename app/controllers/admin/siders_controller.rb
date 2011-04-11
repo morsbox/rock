157a6948a9a3e719bd476af44e1ead5f65ae7219
Mik_Die <MikDiet@gmail.com>
2011-04-11 21:25:15 +0800

    added errors when occur in the sider admin views

--- app/controllers/admin/siders_controller.rb
+++ app/controllers/admin/siders_controller.rb
@@ -5,7 +5,7 @@ class Admin::SidersController < Admin::IndexController
   
   def new
     if params[:sider_type_id] and @sider_type = SiderType.where(:id => params[:sider_type_id] ).first
-      @sider = @sider_type.siders.build
+      @sider = @sider_type.siders.build flash[:sider]
     else
       @sider_types = SiderType.where :enabled => true
       render "select_sider_type"
@@ -17,7 +17,9 @@ class Admin::SidersController < Admin::IndexController
     if @sider.save
       redirect_to admin_siders_path
     else
-      redirect_to new_admin_sider_path(:sider_type_id=>params[:sider_type_id])
+      flash[:sider] = params[:sider]
+      flash[:errors] = @sider.errors
+      redirect_to new_admin_sider_path(:sider_type_id=>params[:sider][:sider_type_id])
     end 
   end
   
@@ -30,6 +32,7 @@ class Admin::SidersController < Admin::IndexController
     if @sider.update_attributes(params[:sider])
       redirect_to admin_siders_path
     else
+      flash[:errors] = @sider.errors
       redirect_to edit_admin_sider_path(@sider)
     end 
   end
