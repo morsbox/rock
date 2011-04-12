d28ef308db9ade586c61c9dd9c075aa07b9edaa5
Mik_Die <MikDiet@gmail.com>
2011-04-12 12:19:28 +0800

    basic sider output and all for siders by now!

--- app/helpers/siders/auth_siders_helper.rb
+++ app/helpers/siders/auth_siders_helper.rb
@@ -4,7 +4,7 @@ module Siders::AuthSidersHelper
   end
   def self.default_params
     { :show_title => {:type => 'check', :value => '1'},
-      :greeting_msg => {:type => 'string', :value => 'Hello, #{current_user.username}'}}
+      :greeting_msg => {:type => 'string', :value => 'Hello!'}}
   end
   
   def self.output_method
@@ -14,6 +14,27 @@ end
 
 module ApplicationHelper
   def auth_siders_output(p)
-    "<div class='sider_#{p[:sider_position]}'>Content of #{p[:title]}</div>"
+    title = "<h3 class='auth_title'>#{p[:title]}</h3>" if p[:show_title]=='1'
+    if user_signed_in?
+      form = "<p>#{p[:greeting_msg]}</p>"
+      form+= link_to "Sign out, #{current_user.username}", destroy_user_session_path
+    else
+      form = capture{form_for("user", :url => user_session_path) do |f|
+        concat f.text_field :username 
+        concat f.password_field :password
+        concat f.check_box :remember_me
+        concat f.label :remember_me
+        concat f.submit 'Sign in'
+        link_to "Forgot your password?", new_password_path('user')
+      end}
+    end
+    <<-HTML
+      <div class='sider_#{p[:sider_position]}'>
+      #{title}
+      <div class='auth_title'>
+      #{form}
+      </div>
+      </div>
+    HTML
   end
 end
--- app/helpers/siders/static_siders_helper.rb
+++ app/helpers/siders/static_siders_helper.rb
@@ -4,6 +4,7 @@ module Siders::StaticSidersHelper
   end
   def self.default_params
     { :content => {:type => 'text', :cols => 35, :rows => 15, :value => ''},
+      :sider_id => {:type => 'string', :value => ''},
       :show_title => {:type => 'check', :value => '1'}}
   end
   
@@ -14,11 +15,12 @@ end
 
 module ApplicationHelper
   def static_siders_output(p)
-    title = "<h3>#{p[:title]}</h3>" if p[:show_title]
+    title = "<h3>#{p[:title]}</h3>" if p[:show_title]=='1'
+    sider_id = " id='#{p[:sider_id]}'" unless p[:sider_id].blank?
     <<-HTML
-      <div class='sider_#{p[:sider_position]}'>
+      <div#{sider_id} class='sider_#{p[:sider_position]}'>
       #{title}
-      Content of #{p[:title]}
+      <div>#{p[:content]}</div>
       </div>
     HTML
   end
