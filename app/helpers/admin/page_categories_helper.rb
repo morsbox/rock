2eb9dae22a6f7e5da9ea0ccc8cc863f3ba163e93
Mik_Die <MikDiet@gmail.com>
2011-04-13 01:50:24 +0800

    very easy category tree with edit forms

--- app/helpers/admin/page_categories_helper.rb
+++ app/helpers/admin/page_categories_helper.rb
@@ -1,7 +1,26 @@
 module Admin::PageCategoriesHelper
   def draw_graph parent_category=nil, level = 0
-    result = parent_category ? "<div style='padding-left:#{level*10}px'>#{@categories[parent_category].title}</div>" : ""
+    result = ""
+    if parent_category
+      result+="<div style='padding-left:#{level*10}px'>"
+      result+= form_for [:admin, @categories[parent_category]] do |f|
+        concat f.text_field :title
+        @options_tree ||= [["root", nil]]+draw_options_tree
+        concat f.select :parent_category_id, @options_tree
+        concat f.submit
+        concat link_to "delete", admin_page_category_path(@categories[parent_category]), :method => :delete, :confirm => "Are you sure?"
+      end
+      result+="</div>"
+    end
     @categories_graph.each{ |k,v| result+= draw_graph(k, level+1) if v==parent_category}
+    result
+  end
+  
+  # return array of pairs [title,id] recursive
+  def draw_options_tree parent_category=nil, level = 0
+    result = []
+    result << ["_"*level+@categories[parent_category].title, parent_category] if parent_category
+    @categories_graph.each{ |k,v| result+= draw_options_tree k, level+1 if v==parent_category}
     result
   end
 end
