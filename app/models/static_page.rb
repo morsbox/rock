7dbf42c8ec56a0a0f21cb61d49030fafa0826158
Mik_Die <MikDiet@gmail.com>
2011-05-10 12:26:21 +0800

    instanse method link for StaticPage, which returned path to page depended on locale code

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,7 +1,15 @@
 class StaticPage < ActiveRecord::Base
+  include R18n::Translated
+  
   belongs_to :author, :class_name=>"User"
   belongs_to :page_category
   
   validates_presence_of :title
   #TODO validates :url_match
+  
+  def link locale=nil
+    locale ||= I18n.locale
+    prefix = locale==I18n.default_locale ? "" : "/#{locale.to_s}"
+    prefix + "/" + url_match
+  end
 end
